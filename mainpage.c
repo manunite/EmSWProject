950c3c72f01a27f133c064532c55d205b898034f
Jisu Hwang <zsueee@gmail.com>
2015-11-22 21:47:44 -0800

    jisu

--- mainpage.c
+++ mainpage.c
@@ -68,9 +68,9 @@ int main(int argc, char *argv[]) {
   GtkWidget *table;
 
   GtkWidget *info;
-  GtkWidget *warn;
-  GtkWidget *que;
-  GtkWidget *err;
+  GtkWidget *join;
+  GtkWidget *start;
+  GtkWidget *quit;
 
   gtk_init(&argc, &argv);
 
@@ -84,17 +84,18 @@ int main(int argc, char *argv[]) {
   gtk_table_set_col_spacings(GTK_TABLE(table), 2);
 
   info = gtk_button_new_with_label("Info");
-  warn = gtk_button_new_with_label("Warning");
-  que = gtk_button_new_with_label("Question");
-  err = gtk_button_new_with_label("Error");
+  join = gtk_button_new_with_label("Join");
+start = gtk_button_new_with_label("Start");  
+quit = gtk_button_new_with_label("Quit");
+  
 
   gtk_table_attach(GTK_TABLE(table), info, 0, 1, 0, 1, 
       GTK_FILL, GTK_FILL, 3, 3);
-  gtk_table_attach(GTK_TABLE(table), warn, 1, 2, 0, 1, 
+  gtk_table_attach(GTK_TABLE(table), join, 1, 2, 0, 1, 
       GTK_FILL, GTK_FILL, 3, 3);
-  gtk_table_attach(GTK_TABLE(table), que, 0, 1, 1, 2, 
+  gtk_table_attach(GTK_TABLE(table), start, 0, 1, 1, 2, 
       GTK_FILL, GTK_FILL, 3, 3);
-  gtk_table_attach(GTK_TABLE(table), err, 1, 2, 1, 2, 
+  gtk_table_attach(GTK_TABLE(table), quit, 1, 2, 1, 2, 
       GTK_FILL, GTK_FILL, 3, 3);
   
   gtk_container_add(GTK_CONTAINER(window), table);
@@ -103,13 +104,13 @@ int main(int argc, char *argv[]) {
   g_signal_connect(G_OBJECT(info), "clicked", 
         G_CALLBACK(show_info), (gpointer) window); 
 
-  g_signal_connect(G_OBJECT(warn), "clicked", 
+  g_signal_connect(G_OBJECT(join), "clicked", 
         G_CALLBACK(show_warning), (gpointer) window); 
 
-  g_signal_connect(G_OBJECT(que), "clicked", 
+  g_signal_connect(G_OBJECT(quit), "clicked", 
         G_CALLBACK(show_question), (gpointer) window); 
 
-  g_signal_connect(G_OBJECT(err), "clicked", 
+  g_signal_connect(G_OBJECT(start), "clicked", 
         G_CALLBACK(show_error), (gpointer) window); 
 
   g_signal_connect(G_OBJECT(window), "destroy",
